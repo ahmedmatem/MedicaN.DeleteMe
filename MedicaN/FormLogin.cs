80a27343db3114e832340a83016e99f23fceb3f0
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-04-01 10:45:48 +0300

    Hash password

--- MedicaN/FormLogin.cs
+++ MedicaN/FormLogin.cs
@@ -1,6 +1,7 @@
 using MedicaN.Data;
 using MedicaN.Data.Models;
-using Microsoft.AspNet.Identity;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace MedicaN
 {
@@ -16,24 +17,50 @@ namespace MedicaN
             var username = textBoxUsername.Text;
             var password = textBoxPassword.Text;
             var user = Login(username, password);
+            if(user is null)
+            {
+                // login failed
+                MessageBox.Show("Login failed. Try again.");
+            }
+            else
+            {
+                // login successful
+                switch (user.Role.RoleName)
+                {
+                    case "Admin":
+                        MessageBox.Show("Go to Admin panel.");
+                        break;
+                    case "Doctor":
+                        MessageBox.Show("Go to Doctor panel.");
+                        break;
+                    default:
+                        MessageBox.Show("Error: undefined role.");
+                        break;
+                }               
+            }
         }
 
         private User? Login(string username, string password)
         {
-            HashPassword(password);
             using (var context = new HealthNdbContext())
             {
                 return context.Users.FirstOrDefault(u =>
                     u.Username == username &&
-                    u.PasswordHash == password);
+                    u.PasswordHash == HashPassword(password));
             }
         }
 
         private string HashPassword(string password)
         {
-            var hasher = new PasswordHasher();
-            var hashedPassword = hasher.HashPassword(password);
-            return hashedPassword;
+            byte[] bytes = Encoding.UTF8.GetBytes(password);
+            byte[] hashBytes = SHA256.HashData(bytes);
+
+            StringBuilder sb = new StringBuilder(hashBytes.Length * 2);
+            foreach (byte b in hashBytes)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
         }
     }
 }
